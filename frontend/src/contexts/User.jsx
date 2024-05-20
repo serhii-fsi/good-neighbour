259c8a813c89927326d155a4da167e796033f4d0
Kam Cheng <kam.cheng03@gmail.com>
2024-05-20 21:30:35 +0100

    create User context

---
+++ frontend/src/contexts/User.jsx
@@ -0,0 +1,15 @@
+import { createContext } from "react";
+
+export const UserContext = createContext()
+
+function UserProvider({ children }) {
+    const [user, setUser] = useState("");
+
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            { children }
+        </UserContext.Provider>
+    )
+}
+
+export default UserProvider;
