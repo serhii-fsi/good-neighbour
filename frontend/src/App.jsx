520821443a47acdbe33da2eb7353a1a3a45d792d
Kam Cheng <kam.cheng03@gmail.com>
2024-05-20 17:59:33 +0100

    SignupLogin component display buttons in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
+import SignupLogin from "./pages/SignupLogin";
 
 function App() {
   return (
     <>
       <Header />
+      <SignupLogin />
       <Routes>
         <Route></Route>
       </Routes>
--- frontend/src/pages/SignupLogin.jsx
+++ frontend/src/pages/SignupLogin.jsx
@@ -1,8 +1,22 @@
+import { Link, Routes, Route } from 'react-router-dom';
+import SignupForm from '../components/SignupForm';
+import LoginForm from '../components/LoginForm';
+
 function SignupLogin() {
     return (
     <>
-    <button type="button" class="block">Sign up</button>
-    <button type="button" class="block">Log in</button>
+    <Link to="/signup" >
+    <button type="button" className="block">Sign up</button>
+    </Link>
+    <Link to="/login">
+    <button type="button" className="block">Log in</button>
+    </Link>
+    <div className='column'>
+    <Routes>
+    <Route path="/signup" element={<SignupForm /> } />
+    <Route path="/login" element={<LoginForm /> }  />
+    </Routes>
+    </div>
     </>
     )
 }
