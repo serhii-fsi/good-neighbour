4fe3dffc71fc6d73d2e640be540aea1a2525a761
Maksim Lukianenko <maxbuzz8694@gmail.com>
2024-05-20 19:52:46 +0100

    chore: update seed script to use new file structure and run-seed.ts file

--- backend/app/db/schema/users.sql
+++ backend/app/db/schema/users.sql
@@ -1,13 +1,12 @@
-DROP TABLE IF EXISTS users;
-
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
+    username VARCHAR NOT NULL,
     avatar_url VARCHAR NOT NULL,
     first_name VARCHAR NOT NULL,
-    surname VARCHAR NOT NULL,
-    user_address VARCHAR NOT NULL,
-    user_age INT NOT NULL,
-    residence_area VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    email VARCHAR NOT NULL,
+    address VARCHAR NOT NULL,
+    age INT NOT NULL,
     post_code VARCHAR NOT NULL,
     help_offered INT NOT NULL,
     help_requests INT NOT NULL
