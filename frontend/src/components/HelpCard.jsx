7989d9c91ffae29aed849dcabddc91cd88ab261d
Shaquille Kiragu <skiragu77@gmail.com>
2024-05-20 22:59:42 +0100

    Updating HelpList and commenting out the HelpCard component until the endpoint is available

--- frontend/src/components/HelpCard.jsx
+++ frontend/src/components/HelpCard.jsx
@@ -1,13 +1,13 @@
-function HelpCard({ request }) {
-  return (
-    <section>
-      <h3>{request.request_title}</h3>
-      <p>Description: {request.request_body}</p>
-      <p>Date(s): {request.request_date}</p>
-      <p>Created at: {request.created_at}</p>
-      <p>Location: {request.location}</p>
-    </section>
-  );
-}
+// function HelpCard({ request }) {
+//   return (
+//     <section>
+//       <h3>{request.request_title}</h3>
+//       <p>Description: {request.request_body}</p>
+//       <p>Date(s): {request.request_date}</p>
+//       <p>Created at: {request.created_at}</p>
+//       <p>Location: {request.location}</p>
+//     </section>
+//   );
+// }
 
-export default HelpCard;
+// export default HelpCard;
--- frontend/src/components/HelpListView.jsx
+++ frontend/src/components/HelpListView.jsx
@@ -1,14 +1,18 @@
-import HelpCard from "./HelpCard";
+// import HelpCard from "./HelpCard";
 
 function HelpListView({ helpList }) {
   return (
-    <ul>
-      {helpList.map((request) => {
-        return (
-          <li key={request.request_id}>{<HelpCard request={request} />}</li>
-        );
-      })}
-    </ul>
+    <p>
+      *Placeholder for helplist. Waiting for endpoint to be available from the
+      backend.*
+    </p>
+    // <ul>
+    //   {helpList.map((request) => {
+    //     return (
+    //       <li key={request.request_id}>{<HelpCard request={request} />}</li>
+    //     );
+    //   })}
+    // </ul>
   );
 }
 
