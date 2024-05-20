fadd191e4ff26e83723d592aabb483d8b07751fa
Shaquille Kiragu <skiragu77@gmail.com>
2024-05-20 16:22:59 +0100

    Changed default theme to light, changed the navbar, and refactored the routes on App.jsx to function properly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
-import NavBar from "./NavBar";
+import NavBar from "./components/NavBar";
+import SignUpLogin from "./pages/SignupLogin";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route></Route>
+        <Route path="/" element={<SignUpLogin />} />
       </Routes>
       <NavBar />
     </>
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -5,9 +5,13 @@ function NavBar() {
     <footer>
       <nav>
         <Link to={"/user-profile"}>Profile</Link>
+        <br />
         <Link to={"/helplist"}>Help Requests</Link>
+        <br />
         <Link to={"/map"}>Requests Near You</Link>
+        <br />
         <Link to={"/own-helplist"}>Your Help Requests</Link>
+        <br />
         <Link to={"/own-offeredlist"}>Help You've Offered</Link>
       </nav>
     </footer>
