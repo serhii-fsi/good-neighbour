c57bc929f519738a9d409b1ffe85615010d25f9d
Glawall <glawall217@gmail.com>
2024-05-23 15:09:36 +0100

    BE - completes patch request on api/help-request/:help-request_id

---
+++ backend/__tests__/integration/v1/helpRequests/update.test.ts
@@ -0,0 +1,36 @@
+import request from "supertest";
+import app from "../../../../app/app";
+import db from "../../../../app/db/connection";
+
+import testData from "../../../../app/db/seeds/data/test";
+import seed from "../../../../app/db/seeds/seed";
+
+import { HelpRequest } from "../../../../app/db/seeds/data/types/data.types";
+
+beforeEach(async () => {
+    await seed(testData);
+});
+
+afterAll(async () => {
+    await db.end();
+});
+
+describe("updateUser", () => {
+    test("200 - PATCH: Responds with an updated email and phone_number user's fields by corresponding id", async () => {
+        const helpRequestBody: Partial<HelpRequest> = {
+            title: "A pint of milk please",
+            help_type_id: 2,
+            description: "Can I just get a pint of gold top please?",
+            req_date: "2024-05-23T10:27:31.575Z",
+        };
+        const {
+            body: { updatedHelpRequest },
+        } = await request(app).patch("/api/help-requests/3").send(helpRequestBody).expect(200);
+        expect(updatedHelpRequest).toMatchObject({
+            title: "A pint of milk please",
+            help_type_id: 2,
+            description: "Can I just get a pint of gold top please?",
+            req_date: expect.any(String),
+        });
+    });
+});
