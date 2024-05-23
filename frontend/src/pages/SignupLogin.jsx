b1c61382ca9999ab36eb393501d8f8b29006315b
Kam Cheng <kam.cheng03@gmail.com>
2024-05-23 11:35:41 +0100

    refactor SignupLogin component in line with ant-design

--- frontend/src/pages/SignupLogin.jsx
+++ frontend/src/pages/SignupLogin.jsx
@@ -1,14 +1,27 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { Button, Flex } from "antd";
 
 function SignupLogin() {
+
+    const navigate = useNavigate();
+
+    function handleLoginClick() {
+        navigate('/login');
+    }
+
+    function handleSignupClick() {
+        navigate('/signup');
+    }
+
     return (
     <>
-    <Link to="/signup" >
-    <button type="button" className="block">Sign up</button>
-    </Link>
-    <Link to="/login">
+        <Flex gap="small" style={{ justifyContent: "center", alignItems: "center" }} wrap>
+            <Button type="primary" onClick={handleLoginClick}>Login</Button>
+            <Button onClick={handleSignupClick}>Signup</Button>
+        </Flex>
+        {/* <Link to="/login">
     <button type="button" className="block">Log in</button>
-    </Link>
+    </Link> */}
     </>
     )
 }
