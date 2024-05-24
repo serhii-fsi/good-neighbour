43fc782b39c0194914b6543d3df8059dc6a5ebf1
Kam Cheng <kam.cheng03@gmail.com>
2024-05-24 11:16:09 +0100

    Header renders either DropdownMenu or SignupLogin based on UserContext

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,22 @@
 import { useContext } from "react";
 import { UserContext } from "../contexts/User";
 import DropdownMenu from "./Menu";
+import SignupLogin from "../pages/SignupLogin";
 
 function Header() {
   const { user } = useContext(UserContext);
-
-  return (
-    <>
-      <h1>Good Neighbour {user}</h1>
-      <DropdownMenu />
-    </>
-  );
+  if (user) {return (
+  <>
+    <h1>Good Neighbour {user}</h1>
+    <DropdownMenu />
+  </>
+  )}
+  else {return (
+  <>
+    <h1>Good Neighbour</h1>
+    <SignupLogin />
+  </>
+  )}
 }
 
 export default Header;
