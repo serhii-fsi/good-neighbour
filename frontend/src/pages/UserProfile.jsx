4a650c2ecd01e9b2a4c2f8d157fa5bbb443630d3
Shaquille Kiragu <skiragu77@gmail.com>
2024-05-24 15:40:06 +0100

    Homepage Design Complete

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -1,5 +1,35 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getUserProfile } from "../api";
+import Loading from "../presentations/Loading";
+
 function UserProfile() {
-  return <p>*UserProfile placeholder*</p>;
+  const { user_id } = useParams();
+  console.log(user_id, "user_id");
+
+  const [profileObj, setProfileObj] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getUserProfile(user_id)
+      .then((response) => {
+        setProfileObj(response.data.usersData, "data");
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <Loading text={"profile"} />;
+  }
+  return (
+    <>
+      <h2>{} Profile</h2>
+      <p>*UserProfile placeholder*</p>
+    </>
+  );
 }
 
 export default UserProfile;
