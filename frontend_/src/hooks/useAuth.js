30b9e62c74e53149428644a156434311dddae7b0
Maksim Lukianenko <maxbuzz8694@gmail.com>
2024-05-25 16:42:01 +0100

    chore(index.html): update script source to point to main.jsx instead of App.jsx for consistency with file name feat(App.jsx): refactor App component to use AuthContext and useAuth hook for authentication functionality feat(auth-context.js): create AuthContext to provide authentication state and functions to child components feat(useAuth.js): create useAuth hook to handle authentication logic and provide authentication state and functions feat(main.jsx): create main.jsx as the entry point of the frontend application, rendering the App component wrapped in BrowserRouter and StrictMode

---
+++ frontend_/src/hooks/useAuth.js
@@ -0,0 +1,29 @@
+import { useState, useEffect, useCallback } from "react";
+
+export const useAuth = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [user, setUser] = useState({});
+
+    const login = useCallback((user) => {
+        const userJSON = JSON.stringify(user);
+        localStorage.setItem("user", userJSON);
+        setUser(user);
+        setIsLoggedIn(true);
+    }, []);
+
+    const logout = useCallback(() => {
+        localStorage.removeItem("user");
+        setIsLoggedIn(false);
+        setUser({});
+    }, []);
+
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem("user"));
+        if (user) {
+            setUser(user);
+            setIsLoggedIn(true);
+        }
+    }, []);
+
+    return { isLoggedIn, login, logout, user };
+};
