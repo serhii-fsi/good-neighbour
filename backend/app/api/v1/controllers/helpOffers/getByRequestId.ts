123829b3bad5424f045f5da86dd4efb9c2e9439a
Kam Cheng <kam.cheng03@gmail.com>
2024-05-27 14:58:41 +0100

    complete refactor of getByRequestId to include empty case

--- backend/app/api/v1/controllers/helpOffers/getByRequestId.ts
+++ backend/app/api/v1/controllers/helpOffers/getByRequestId.ts
@@ -1,19 +1,21 @@
 import { Request, Response, NextFunction } from "express";
 
 import * as helpOffersService from "../../../../services/helpOffers/getByRequestId";
+import * as helpRequestsService from "../../../../services/helpRequests/getById";
 import { AppError } from "../../../../common/errors/AppError";
 import { errors } from "../../../../common/errors/errors";
 
 export const getByRequestId = async (req: Request, res: Response, next: NextFunction) => {
 
     const { help_request_id } = req.params;
-    const requestId = Number(help_request_id);    
-
+    const requestId = Number(help_request_id);
+    
     try {        
         if (isNaN(requestId)) {
             throw new AppError(errors.VALIDATION_ERROR, "Invalid help request id provided");        
         };
-        const helpOffers = await helpOffersService.getByRequestId(Number(help_request_id));
+        await helpRequestsService.getById(requestId);
+        const helpOffers = await helpOffersService.getByRequestId(requestId);
         res.status(200).send({ helpOffers });        
     } catch (error) {
         next(error);
