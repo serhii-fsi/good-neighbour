403e4a8ca6b09255bc4660b07e8d456a173ecaea
Shaquille Kiragu <skiragu77@gmail.com>
2024-05-28 16:11:41 +0100

    Updated bottom NavBar functionality

--- frontend_/src/components/NavBottom/NavBottom.jsx
+++ frontend_/src/components/NavBottom/NavBottom.jsx
@@ -1,63 +1,10 @@
-import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
-import {
-    SearchOutlined,
-    ToTopOutlined,
-    PlusCircleOutlined,
-    VerticalAlignBottomOutlined,
-} from "@ant-design/icons";
 import config from "../../config.json";
 import NavBottomView from "./NavBottomView";
 
 const NavBottom = () => {
     const { routes } = config;
-    const menuItems = [
-        {
-            key: "1",
-            label: (
-                <Link id="navlink1" to={routes.offerHelpPage.path}>
-                    <SearchOutlined style={{ width: "50px" }} />
-                </Link>
-            ),
-        },
-        {
-            key: "2",
-            label: (
-                <Link id="navlink2" to={routes.myOffersPage.path}>
-                    <ToTopOutlined style={{ width: "50px" }} />
-                </Link>
-            ),
-        },
-        {
-            key: "3",
-            label: (
-                <Link id="navlink3" to={routes.requestCreatePage.path}>
-                    <PlusCircleOutlined style={{ width: "50px" }} />
-                </Link>
-            ),
-        },
-        {
-            key: "4",
-            label: (
-                <Link id="navlink4" to={routes.myRequestsPage.path}>
-                    <VerticalAlignBottomOutlined style={{ width: "50px" }} />
-                </Link>
-            ),
-        },
-    ];
 
-    const navigate = useNavigate();
-    const handleClick = () => {
-        navigate(-1);
-    };
-
-    return (
-        <NavBottomView
-            className="NavBottom__footer"
-            passedItems={menuItems}
-            handleClick={handleClick}
-        />
-    );
+    return <NavBottomView routes={routes} />;
 };
 
 export default NavBottom;
--- frontend_/src/components/NavBottom/NavBottomView.jsx
+++ frontend_/src/components/NavBottom/NavBottomView.jsx
@@ -1,10 +1,52 @@
+import { Link } from "react-router-dom";
 import { Menu } from "antd";
+import {
+    SearchOutlined,
+    ToTopOutlined,
+    PlusCircleOutlined,
+    VerticalAlignBottomOutlined,
+} from "@ant-design/icons";
 import "./NavBottomView.css";
 
-const NavBottomView = ({ passedItems }) => {
+const NavBottomView = ({ routes }) => {
+    const menuItems = [
+        {
+            key: "1",
+            label: (
+                <Link id="navlink1" to={routes.offerHelpPage.path}>
+                    <SearchOutlined style={{ width: "50px" }} />
+                </Link>
+            ),
+        },
+        {
+            key: "2",
+            label: (
+                <Link id="navlink2" to={routes.myOffersPage.path}>
+                    <ToTopOutlined style={{ width: "50px" }} />
+                </Link>
+            ),
+        },
+        {
+            key: "3",
+            label: (
+                <Link id="navlink3" to={routes.requestCreatePage.path}>
+                    <PlusCircleOutlined style={{ width: "50px" }} />
+                </Link>
+            ),
+        },
+        {
+            key: "4",
+            label: (
+                <Link id="navlink4" to={routes.myRequestsPage.path}>
+                    <VerticalAlignBottomOutlined style={{ width: "50px" }} />
+                </Link>
+            ),
+        },
+    ];
+
     return (
         <footer className="NavBottomView__footer">
-            <Menu id="navbottom" items={passedItems} mode="horizontal" />
+            <Menu id="navbottom" items={menuItems} mode="horizontal" />
         </footer>
     );
 };
