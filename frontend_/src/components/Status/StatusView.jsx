cd12ae7862903509e2db48f962aa2cf0125b8539
Glawall <glawall217@gmail.com>
2024-05-29 17:25:06 +0100

    some status logic

--- frontend_/src/components/Status/StatusView.jsx
+++ frontend_/src/components/Status/StatusView.jsx
@@ -6,16 +6,22 @@
  * @param {string|null} props.grayBoldText
  * @param {string|null} props.linkText
  * @param {string|null} props.linkPath
+ * 
+ *            
  */
+import { Link } from "react-router-dom";
+import "./StatusView.css";
+
+
 export default function StatusView(props) {
     return (
         <>
-            greenText: {props.greenText} <br />
-            greenBoldText: {props.greenBoldText} <br />
-            grayText: {props.grayText} <br />
-            grayBoldText: {props.grayBoldText} <br />
-            linkText: {props.linkText} <br />
-            linkPath: {props.linkPath} <br />
+            {props.greenText ? <p className="greenText">{props.greenText} </p> : null}
+            {props.greenBoldText ? <p className="greenBoldText">{props.greenBoldText} </p> : null}
+            {props.grayText ? <p className="grayText"> {props.grayText} </p> : null}
+            {props.grayBoldText ? <p className="grayBoldText"> {props.grayBoldText}</p> : null}{" "}
+            {props.linkText && props.linkPath ? <Link to = {props.linkPath}>  {props.linkText} </Link> : null}
+            {props.linkPath}
         </>
     );
 }
