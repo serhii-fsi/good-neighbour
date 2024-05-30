40af3f60c553e5db8a762a064f17ee266bd582ab
Shaquille Kiragu <skiragu77@gmail.com>
2024-05-30 11:50:18 +0100

    Done as much as I can to display data. Resorted to displaying original dummy data instead

--- frontend_/src/components/UserProfile/UserProfile.jsx
+++ frontend_/src/components/UserProfile/UserProfile.jsx
@@ -33,36 +33,36 @@ const UserProfile = (props) => {
         console.log(myUserProfile, "myUserProfile");
     }, [props.user?.user_id]);
 
-    // const fields = [
-    //     {
-    //         key: "1",
-    //         label: "About",
-    //         children:
-    //             "Hi! I'm Liam, and I need assistance with grocery shopping once a week. Due to a recent injury, I am unable to carry heavy items. If you have a couple of hours to spare on a Monday or Thursday morning, your help would be greatly appreciated!",
-    //     },
-    //     {
-    //         key: "2",
-    //         label: "Address",
-    //         children: "123 High Street, London, SW1A 1AA, England",
-    //     },
-    //     {
-    //         key: "3",
-    //         label: "Postcode",
-    //         children: "SW1A 1AA",
-    //     },
-    //     {
-    //         key: "4",
-    //         label: "Phone Number",
-    //         span: 2,
-    //         children: "+44 20 7946 0958",
-    //     },
-    //     {
-    //         key: "5",
-    //         label: "Additional information",
-    //         children:
-    //             "Feel free to call me between 10 AM and 6 PM on weekdays, and anytime on weekends. If I'm unavailable, please leave a message, and I'll get back to you as soon as possible.",
-    //     },
-    // ];
+    const fields = [
+        {
+            key: "1",
+            label: "About",
+            children:
+                "Hi! I'm Liam, and I need assistance with grocery shopping once a week. Due to a recent injury, I am unable to carry heavy items. If you have a couple of hours to spare on a Monday or Thursday morning, your help would be greatly appreciated!",
+        },
+        {
+            key: "2",
+            label: "Address",
+            children: "123 High Street, London, SW1A 1AA, England",
+        },
+        {
+            key: "3",
+            label: "Postcode",
+            children: "SW1A 1AA",
+        },
+        {
+            key: "4",
+            label: "Phone Number",
+            span: 2,
+            children: "+44 20 7946 0958",
+        },
+        {
+            key: "5",
+            label: "Additional information",
+            children:
+                "Feel free to call me between 10 AM and 6 PM on weekdays, and anytime on weekends. If I'm unavailable, please leave a message, and I'll get back to you as soon as possible.",
+        },
+    ];
 
     const handleClick = () => {
         console.log("User profile edit btn clicked");
@@ -70,7 +70,7 @@ const UserProfile = (props) => {
 
     return (
         <UserProfileView
-            fields={myUserProfile}
+            fields={fields}
             user={props.user}
             handleClick={handleClick}
             logout={props.logout}
