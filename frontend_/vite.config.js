b1a10ff7648238b4dc0845fc57265b120d938eb1
Maksim Lukianenko <maxbuzz8694@gmail.com>
2024-05-30 09:24:47 +0100

    chore(vite.config.js): comment out origin and host configuration to disable specific network settings

--- frontend_/vite.config.js
+++ frontend_/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [react()],
     server: {
         port: 7777,
-        origin: "http://192.168.0.21:7777",
-        host: "0.0.0.0", // Add this line to allow connections from other devices in the network
+        // origin: "http://192.168.0.21:7777",
+        // host: "0.0.0.0", // Add this line to allow connections from other devices in the network
     },
 });
